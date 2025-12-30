47bd594afeeaa8610534ef909be59c4ba1230cde
ilyasaitbel <ilyasilya084@gmail.com>
2025-12-30 10:04:03 +0100

    database & AuthService

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+class DatabaseConnection
+{
+    private PDO $conn;
+
+    public function __construct(
+        private $host = "localhost",
+        private $db_name = "paiement",
+        private $username = "root",
+        private $password = ""
+    ) {
+        $this->conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+        if (!$this->conn) {
+            die('ne pas connnection rasute');
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
